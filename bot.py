ebb5cf0f43ead8ad480b9d27a8c11a8d864dff8f
moein1388 <168118089+moein1388@users.noreply.github.com>
2025-08-07 20:30:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,9 +81,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
             await update.message.reply_text(bot_reply)
 
-if name == "__main__":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("ربات جغد مودب آماده به کار است!")
     app.run_polling()
+
